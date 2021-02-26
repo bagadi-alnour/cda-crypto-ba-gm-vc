a195181036c2e7c2bffed10bca1a7e3fe009dbb2
valentin-cochin <cochin.valentin@hotmail.fr>
2021-02-26 15:58:39 +0100

    Add CryptoWallet entity

--- src/main/java/com/cda/jee/crypto/model/CryptoWallet.java
+++ src/main/java/com/cda/jee/crypto/model/CryptoWallet.java
@@ -1,5 +1,21 @@
 package com.cda.jee.crypto.model;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CryptoWallet {
-    private  int id;
+	private int id;
+	private String name;
+	private String symbol;
+	private double currentPrice;
+	private String imageUrl;
+	private LocalDateTime lastUpdated;
 }
