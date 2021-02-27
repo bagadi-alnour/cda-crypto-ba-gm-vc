c020494af3cbe0f8d2a004e89b7bde1b6da46aa4
valentin-cochin <cochin.valentin@hotmail.fr>
2021-02-27 19:17:34 +0100

    Add implementations of DAO and personalized exceptions to IDao

--- src/main/java/com/cda/jee/crypto/dao/IDao.java
+++ src/main/java/com/cda/jee/crypto/dao/IDao.java
@@ -3,15 +3,17 @@ package com.cda.jee.crypto.dao;
 import java.util.List;
 import java.util.Optional;
 
+import com.cda.jee.crypto.dao.exception.DaoException;
+
 public interface IDao<T> {
     
-    Optional<T> get(int id);
+    Optional<T> get(int id) throws DaoException;
     
-    List<T> getAll();
+    List<T> getAll() throws DaoException;
     
-    void save(T t);
+    void save(T t) throws DaoException;
     
-    void update(T t, String[] params);
+    void update(T t, String[] params) throws DaoException;
     
-    void delete(T t);
+    void delete(T t) throws DaoException;
 }
--- src/main/java/com/cda/jee/crypto/dao/impl/CryptoCurrencyImpl.java
+++ src/main/java/com/cda/jee/crypto/dao/impl/CryptoCurrencyImpl.java
@@ -1,5 +1,46 @@
 package com.cda.jee.crypto.dao.impl;
 
+<<<<<<< HEAD
 public class CryptoCurrencyImpl {
 	
+=======
+import java.util.List;
+import java.util.Optional;
+
+import com.cda.jee.crypto.dao.CryptoCurrencyDao;
+import com.cda.jee.crypto.dao.exception.DaoException;
+import com.cda.jee.crypto.model.CryptoCurrency;
+
+public class CryptoCurrencyImpl implements CryptoCurrencyDao {
+
+	@Override
+	public Optional<CryptoCurrency> get(int id) throws DaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CryptoCurrency> getAll() throws DaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(CryptoCurrency t) throws DaoException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(CryptoCurrency t, String[] params) throws DaoException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(CryptoCurrency t) throws DaoException {
+		// TODO Auto-generated method stub
+
+	}
+>>>>>>> b75140c... Add implementations of DAO and personalized exceptions to IDao
 }
