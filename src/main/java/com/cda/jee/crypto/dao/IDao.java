187e9b497d96aeac434f1577b8ad6eab6b734fec
valentin-cochin <cochin.valentin@hotmail.fr>
2021-02-27 19:15:55 +0100

    Add IDao. The two dao interfaces extends IDao

---
+++ src/main/java/com/cda/jee/crypto/dao/IDao.java
@@ -0,0 +1,17 @@
+package com.cda.jee.crypto.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IDao<T> {
+    
+    Optional<T> get(int id);
+    
+    List<T> getAll();
+    
+    void save(T t);
+    
+    void update(T t, String[] params);
+    
+    void delete(T t);
+}
