c5bb2fb223d54d06434ea2962e3e2257a32ffc59
valentin-cochin <cochin.valentin@hotmail.fr>
2021-03-01 15:18:07 +0100

    Update scripts for the creation of the database

--- docs/ddl_script.sql
+++ docs/ddl_script.sql
@@ -1,8 +1,9 @@
 use crypto_db;
 set names utf8;
 
-DROP TABLE IF EXISTS `cryptoCurrency`;
 DROP TABLE IF EXISTS `cryptoWallet`;
+DROP TABLE IF EXISTS `cryptoCurrency`;
+
 
 CREATE TABLE `cryptoCurrency` ( 
   `idCrypto` INTEGER NOT NULL AUTO_INCREMENT,
@@ -16,11 +17,15 @@ CREATE TABLE `cryptoCurrency` (
 );
 
 CREATE TABLE `cryptoWallet` (
-    `idWallet` INTEGER not NULL AUTO_INCREMENT,
-    `idCrypto` INTEGER NOT NULL,
-    `purchasePrice` double(30,9) not NULL,
-    `quantity` double(20,19) NOT NULL,
-    `purchaseDate` DATETIME NOT NULL default CURRENT_TIMESTAMP,
-    PRIMARY KEY (`idWallet`),
-    FOREIGN KEY (idCrypto) REFERENCES `cryptoCurrency` (`idCrypto`)
+  `idWallet` INTEGER not NULL AUTO_INCREMENT,
+  `idCrypto` INTEGER NOT NULL,
+  `purchasePrice` double(30,9) not NULL,
+  `quantity` double(30,9) NOT NULL,
+  `purchaseDate` DATETIME NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idWallet`),
+  FOREIGN KEY (idCrypto) REFERENCES `cryptoCurrency` (`idCrypto`)
 );
+​
+​
+​
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
