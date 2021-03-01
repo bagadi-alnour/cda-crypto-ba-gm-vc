a9da2380413eb87b187c73770cb3e59a5a087fee
valentin-cochin <cochin.valentin@hotmail.fr>
2021-03-01 09:48:24 +0100

    Fix the creation of databases for H2

--- src/test/resources/ddl_script.sql
+++ src/test/resources/ddl_script.sql
@@ -1,26 +1,23 @@
-use crypto_db;
-set names utf8;
+DROP TABLE IF EXISTS cryptoCurrency;
+DROP TABLE IF EXISTS cryptoWallet;
 
-DROP TABLE IF EXISTS `cryptoCurrency`;
-DROP TABLE IF EXISTS `cryptoWallet`;
-
-CREATE TABLE `cryptoCurrency` ( 
-  `idCrypto` INTEGER NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50) NOT NULL DEFAULT 'N/D',
-  `symbol` VARCHAR(6) NOT NULL,
-  `delta` DOUBLE(30,9),
-  `currentPrice` double(30,9) NOT NULL,
-  `imageUrl` TINYTEXT,
-  `lastUpdated` DATETIME NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idCrypto`)
+CREATE TABLE cryptoCurrency ( 
+  idCrypto INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+  name VARCHAR(50) NOT NULL DEFAULT 'N/D',
+  symbol VARCHAR(6) NOT NULL,
+  delta DECIMAL(30,9),
+  currentPrice DECIMAL(30,9) NOT NULL,
+  imageUrl VARCHAR(50),
+  lastUpdated TIMESTAMP  NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY (idCrypto)
 );
 
-CREATE TABLE `cryptoWallet` (
-    `idWallet` INTEGER not NULL AUTO_INCREMENT,
-    `idCrypto` INTEGER NOT NULL,
-    `purchasePrice` double(30,9) not NULL,
-    `quantity` double(20,19) NOT NULL,
-    `purchaseDate` DATETIME NOT NULL default CURRENT_TIMESTAMP,
-    PRIMARY KEY (`idWallet`),
-    FOREIGN KEY (idCrypto) REFERENCES `cryptoCurrency` (`idCrypto`)
+CREATE TABLE cryptoWallet (
+    idWallet INTEGER not NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+    idCrypto INTEGER NOT NULL,
+    purchasePrice DECIMAL(30,9) not NULL,
+    quantity DECIMAL(20,19) NOT NULL,
+    purchaseDate TIMESTAMP  NOT NULL default CURRENT_TIMESTAMP,
+    PRIMARY KEY (idWallet),
+    FOREIGN KEY (idCrypto) REFERENCES cryptoCurrency (idCrypto)
 );
